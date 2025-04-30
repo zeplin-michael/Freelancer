345a5153c1e52de7af240a10d3085929e0b03aba
Zeplin Rowe <rowe_michael101@yahoo.com>
2025-04-30 13:44:50 -0400

    Freelancer Forum

--- README.md
+++ README.md
@@ -8,10 +8,11 @@ You've been contracted to build an online platform for freelancers to advertise 
 
 Write code according to the provided prompts. You are highly encouraged to review previous solutions!
 
-1. Write a function that returns a freelancer object with a randomly generated name, occupation, and rate according to the provided constants.
-2. Initialize a state variable to an array of `NUM_FREELANCERS` freelancer objects.
-3. Write a function that returns the average rate of all freelancers in state.
-4. Use that function to initialize a state variable which will store the average rate of all freelancers.
+<!-- 1. Write a function that returns a freelancer object with a randomly generated name, occupation, and rate according to the provided constants. -->
+<!-- 2. Initialize a state variable to an array of `NUM_FREELANCERS` freelancer objects. -->
+<!-- 3. Write a function that returns the average rate of all freelancers in state. -->
+<!-- 4. Use that function to initialize a state variable which will store the average rate of all freelancers. -->
+
 5. Write a component function to represent a single freelancer.
 6. Write a component function to represent an array of freelancers.
 7. Write a component function to represent the average rate of all freelancers.
--- index.js
+++ index.js
@@ -10,3 +10,71 @@ const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+function freelancerObject() {
+  const name = NAMES[Math.floor(Math.random() * NAMES.length)];
+  const occupation =
+    OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
+  // Why does adding PRICE_RANGE before the brackets affect the result
+  const rate = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+  return { name, occupation, rate };
+}
+console.log(freelancerObject());
+
+const freelancers = Array.from({ length: NUM_FREELANCERS }, freelancerObject);
+console.log(freelancers);
+
+function averageRate(freelancers) {
+  let sum = 0;
+  for (let i = 0; i < freelancers.length; i++) {
+    sum += freelancers[i].rate;
+  }
+  return sum / freelancers.length;
+}
+console.log(averageRate(freelancers));
+
+const allAverageRates = averageRate(freelancers);
+console.log(allAverageRates);
+
+function singleFreelancerComponent({ name, occupation, rate }) {
+  const tableRow = document.createElement("tr");
+  tableRow.innerHTML = ` 
+  <tr>
+      <td> ${name} </td>
+      <td> ${occupation} </td>
+      <td> ${rate} </td>
+  </tr>
+  `;
+  return tableRow;
+}
+
+function assigningRow() {
+  const tbody = document.createElement("tbody");
+  const freelancerRows = freelancers.map(singleFreelancerComponent);
+  tbody.replaceChildren(...freelancerRows);
+  return tbody;
+}
+
+function render() {
+  const app = document.querySelector("#app");
+  app.innerHTML = `
+  <h1>Freelancer Forum</h1>
+  <h3 id = "table-head"> The average rate is ${allAverageRates} </h3>
+  <table>
+    <thead>
+      <tr>
+        <th>NAME</th>
+        <th>OCCUPATION</th>
+        <th>RATE</th>
+      </tr>
+    </thead>
+    <tbody id = "table-body">
+
+
+    </tbody>
+  </table>
+  `;
+  app.querySelector("#table-body").replaceWith(assigningRow());
+}
+
+render();
